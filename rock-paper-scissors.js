2f15e99292ab73140cc10b71fcbc4c5d8f930665
Jacob <stobnicki.jake@gmail.com>
2025-05-21 19:28:52 -0400

    Implemented getComputerChoice function to randomly select rock, paper, or scissors.

---
+++ rock-paper-scissors.js
@@ -0,0 +1,16 @@
+console.log(getComputerChoice())
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice === 0) {
+        return "rock"
+    } else if (choice === 1) {
+        return "paper"
+    } else if (choice === 2) {
+        return "scissors"
+    }
+}
+
+function getHumanChoice(input) {
+    let choice = 
+}
