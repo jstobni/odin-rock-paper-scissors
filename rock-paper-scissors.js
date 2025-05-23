ead7806b184a05db776aaf1a67073c47c17723b5
Jacob <stobnicki.jake@gmail.com>
2025-05-22 21:57:01 -0400

    Finished coding rock paper scissors game and UI.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,20 +1,26 @@
 // Check that DOM is loaded
 document.addEventListener('DOMContentLoaded', () => {
     let rock = document.querySelector('#rock')
-    rock.addEventListener('click', function() {
-        playRound(rock.dataset.choice, getComputerChoice())
-    })
-
+    rock.onclick = function() {
+            playRound(rock.dataset.choice, getComputerChoice())
+        }
     let paper = document.querySelector('#paper')
-    paper.addEventListener('click', function() {
-        playRound(paper.dataset.choice, getComputerChoice())
-    })
-
+    paper.onclick = function() {
+            playRound(paper.dataset.choice, getComputerChoice())
+        }
     let scissors = document.querySelector('#scissors')
-    scissors.addEventListener('click', function() {
-        playRound(scissors.dataset.choice, getComputerChoice())
-    })
+    scissors.onclick = function() {
+            playRound(scissors.dataset.choice, getComputerChoice())
+        }
+    document.addEventListener('click', (event) => {
+    if (event.target.matches('#reset')) {
+        window.location.reload();
+    }
 });
+});
+
+let humanScore = 0;
+let computerScore = 0;
 
 
 function getComputerChoice() {
@@ -29,35 +35,60 @@ function getComputerChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
+    let result = document.querySelector('#result')
+    let humanScoreDisplay = document.querySelector('#humanScore')
+    let computerScoreDisplay = document.querySelector('#computerScore')
+    let buttons = document.querySelector('#buttons')
+
     if (humanChoice === "rock") {
         if (computerChoice === "rock") {
-            console.log("Tie!")
+            result.textContent = "Tie!"
         } else if (computerChoice === "paper") {
-            console.log("You lose! Paper beats Rock.")
-            return "loss"
+            result.textContent = "You lose! Paper beats Rock."
+            computerScore += 1
+            computerScoreDisplay.textContent = computerScore
         } else if (computerChoice === "scissors") {
-            console.log("You win! Rock beats Scissors.")
-            return "win"
+            result.textContent = "You win! Rock beats Scissors."
+            humanScore += 1
+            humanScoreDisplay.textContent = humanScore
         }
     } else if (humanChoice === "paper") {
         if (computerChoice === "rock") {
-            console.log("You win! Paper beats Rock.")
-            return "win"
+            result.textContent = "You win! Paper beats Rock."
+            humanScore += 1
+            humanScoreDisplay.textContent = humanScore
         } else if (computerChoice === "paper") {
-            console.log("Tie!")
+            result.textContent = "Tie!"
         } else if (computerChoice === "scissors") {
-            console.log("You lose! Scissors beats Paper.")
-            return "loss"
+            result.textContent = "You lose! Scissors beats Paper."
+            computerScore += 1
+            computerScoreDisplay.textContent = computerScore
         }
     } else if (humanChoice === "scissors") {
         if (computerChoice === "rock") {
-            console.log("You lose! Rock beats Scissors.")
-            return "loss"
+            result.textContent = "You lose! Rock beats Scissors."
+            computerScore += 1
+            computerScoreDisplay.textContent = computerScore
         } else if (computerChoice === "paper") {
-            console.log("You win! Scissors beats paper.")
-            return "win"
+            result.textContent = "You win! Scissors beats paper."
+            humanScore += 1
+            humanScoreDisplay.textContent = humanScore
         } else if (computerChoice === "scissors") {
-            console.log("Tie!")
+            result.textContent = "Tie!"
         }
-    }   
+    }
+    if (humanScore === 5) {
+        result.textContent = `You won ${humanScore} to ${computerScore}!`
+        rock.disabled = true
+        paper.disabled = true
+        scissors.disabled = true
+        buttons.innerHTML += "<button id=\"reset\">Reset</button>"
+    } else if (computerScore === 5) {
+        result.textContent = `You lost ${computerScore} to ${humanScore}!`
+        rock.disabled = true
+        paper.disabled = true
+        scissors.disabled = true
+        buttons.innerHTML += "<button id=\"reset\">Reset</button>"
+    }
+
 }
